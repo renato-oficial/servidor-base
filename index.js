40b37e1b974882127cf5ea271bccb81c6cb31a11
renato-oficial <128916817+renato-oficial@users.noreply.github.com>
2023-04-24 22:12:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const WAConnection = require('./WAConnection')
 
 app.get("/", (req, res) => res.status(200).json({ message: 'API is alive ;)'}))
-
+app.get("/new", (req, res) => {
+	const waConnection = new WAConnection()
+	waConnection.initialize({ client: { id: 'ec2e7583-9135-480c-97e1-4ff7bbd518e8' }})
+	res.status(200).json({ message: 'Initialized!'})
+})
 
 
 const listener = app.listen( process.env.PORT || 3000, () => {
